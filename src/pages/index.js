2b032555a10e83fa0f4f5923b9ec579a362d9ada
Willy <wavargas9@misena.edu.co>
2023-01-23 15:31:00 -0500

    portafolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,17 @@ export default function Home() {
           id="portafolio"
           className="grid grid-flow-row md:grid-flow-col gap-4"
         >
-          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
+          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
+          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
+          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
+        </div>
+        <div
+          id="portafolio"
+          className="grid grid-flow-row md:grid-flow-col gap-4 mt-10"
+        >
+          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
+          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
+          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
         </div>
       </Layout>
     </>
