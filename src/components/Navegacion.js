8a8528c5d420df344ce5aeaa84b22dc4457c8667
Willy <wavargas9@misena.edu.co>
2023-01-22 21:10:45 -0500

    Navegacion

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -3,9 +3,9 @@ import React from "react";
 
 export default function Navegacion() {
   return (
-    <header className="bg-gradient-to-b from-black to-gray-500">
+    <header className="relative bg-gradient-to-b from-black to-gray-500">
       <nav className="columns-2">
-        <div className="w-24 md:w-56">
+        <div className="w-28 md:w-56">
           <Image
             src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674436209/Zhm_Distribuidores/Logo_Diego_wi1p7e.png"
             alt="Logo"
