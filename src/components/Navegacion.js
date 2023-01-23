696dea3a22f2efbb70cb7a2fdd977c74e0e3a64f
Willy <wavargas9@misena.edu.co>
2023-01-23 13:58:19 -0500

    Varios

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -22,7 +22,7 @@ export default function Navegacion() {
               <Link href="/">Inicio</Link>
             </li>
             <li>
-              <Link href="#portafolio6">Portafolio</Link>
+              <Link href="#portafolio">Portafolio</Link>
             </li>
             <li>
               <Link href="/">Contacto</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,18 @@
 import Layout from "@/components/Layout";
 import Navegacion from "@/components/Navegacion";
+import Image from "next/image";
 import InicioVideo from "../components/InicioVideo";
 
 export default function Home() {
   return (
     <>
       <Layout title="Inicio">
-        <div className="grid">
-          <div className="max-w-sm w-full lg:max-w-full lg:flex">
-            <div
-              className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden fondo1"
-              title="Woman holding a mug"
-            />
-            <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-              <div className="mb-8">
-                <p className="text-sm text-gray-600 flex items-center">
-                  <svg
-                    className="fill-current text-gray-500 w-3 h-3 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                  </svg>
-                  Members only
-                </p>
-                <div className="text-gray-900 font-bold text-xl mb-2">
-                  Can coffee make you a better developer?
-                </div>
-                <p className="text-gray-700 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptatibus quia, nulla! Maiores et perferendis eaque,
-                  exercitationem praesentium nihil.
-                </p>
-              </div>
-              <div className="flex items-center">
-                <img
-                  className="w-10 h-10 rounded-full mr-4"
-                  src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674436209/Zhm_Distribuidores/Logo_Diego_wi1p7e.png"
-                  alt="Avatar of Jonathan Reinink"
-                />
-                <div className="text-sm">
-                  <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-                  <p className="text-gray-600">Aug 18</p>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="text-center my-4 text-2xl">Nuestro Portafolio</div>
+        <div
+          id="portafolio"
+          className="grid grid-flow-row md:grid-flow-col gap-4"
+        >
+          <div className="grid border border-red-800 rounded-xl tarjeta1" />
         </div>
       </Layout>
     </>
