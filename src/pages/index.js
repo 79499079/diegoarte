89a34c0c61129b683fa3c6c9289f986dd6370e01
Willy <wavargas9@misena.edu.co>
2023-01-23 15:40:42 -0500

    portafolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,22 @@ export default function Home() {
         <div className="text-center my-4 text-2xl">Nuestro Portafolio</div>
         <div
           id="portafolio"
-          className="grid grid-flow-row md:grid-flow-col gap-4"
+          className="grid grid-flow-row md:grid-flow-col gap-2"
         >
-          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
-          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
-          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
-        </div>
-        <div
-          id="portafolio"
-          className="grid grid-flow-row md:grid-flow-col gap-4 mt-10"
-        >
-          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
-          <div className="grid border border-red-800 rounded-xl tarjeta1 rotate-3 hover:rotate-0" />
-          <div className="grid border border-red-800 rounded-xl tarjeta1 -rotate-3 hover:rotate-0" />
+          <div className="flex gap-2">
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+          </div>
+
+          <div className="flex gap-2">
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+          </div>
+
+          <div className="flex gap-2">
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
+          </div>
         </div>
       </Layout>
     </>
