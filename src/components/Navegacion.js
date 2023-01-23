a0829c5ff8d00c076e5702bc3f35ce7a44104b04
Willy <wavargas9@misena.edu.co>
2023-01-22 21:16:55 -0500

    Navegacion

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navegacion() {
   return (
     <header className="relative bg-gradient-to-b from-black to-gray-500">
-      <nav className="columns-2">
+      <nav className="columns-2 justify-between">
         <div className="w-28 md:w-56">
           <Image
             src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674436209/Zhm_Distribuidores/Logo_Diego_wi1p7e.png"
@@ -13,9 +13,24 @@ export default function Navegacion() {
             height={200}
           />
         </div>
-        <div className="columns-2">
-          <div>Hola</div>
-          <div>Hola</div>
+        <div className="mr-4 pt-2">
+          <ul class="flex gap-2 justify-end">
+            <li>
+              <a class="text-blue-200 hover:text-blue-500" href="#">
+                Active
+              </a>
+            </li>
+            <li>
+              <a class="text-blue-200 hover:text-blue-500" href="#">
+                Link
+              </a>
+            </li>
+            <li>
+              <a class="text-blue-200 hover:text-blue-500" href="#">
+                Link
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
