d1a365ac23522e5d28d3aedb0f77fa96e06eda85
Willy <wavargas9@misena.edu.co>
2023-01-23 20:09:57 -0500

    Varios

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,47 @@ export default function Home() {
         <div className="text-center my-4 text-2xl">Nuestro Portafolio</div>
         <div
           id="portafolio"
-          className="grid grid-flow-row md:grid-flow-col lg:grid-flow-col gap-4"
+          className="grid grid-flow-row md:grid-flow-col lg:grid-flow-col gap-4 hover:border-transparent hover:text-teal-500 hover:bg-white"
         >
-          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
-          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
-          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              alt="image"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                  Tarjetas
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              alt="image"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                  Tarjetas
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              alt="image"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                  Tarjetas
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
