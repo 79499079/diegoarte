b6cce81691fb5486b3ab8f33013393694ec76683
Willy <wavargas9@misena.edu.co>
2023-01-23 21:20:54 -0500

    portafolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,23 @@
 import Layout from "@/components/Layout";
-/* import Navegacion from "@/components/Navegacion";
-import Image from "next/image";
-import InicioVideo from "../components/InicioVideo"; */
 
 export default function Home() {
   return (
     <>
       <Layout title="Inicio">
-        <div className="text-center my-4 text-2xl">Nuestro Portafolio</div>
+        <div className="text-center my-6 text-2xl">Nuestro Portafolio</div>
         <div
           id="portafolio"
           className="grid grid-flow-row md:grid-flow-col lg:grid-flow-col gap-4 hover:border-transparent hover:text-teal-500 hover:bg-white"
         >
           <div className="w-full rounded justify-center items-center">
             <img
-              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674496475/Zhm_Distribuidores/modelo_tarjeta_iauqsj.jpg"
               alt="image"
+              className="imagenportafolio"
             />
             <div class="grid justify-items-stretch -mt-8">
               <div class="justify-self-center">
-                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
                   Tarjetas
                 </button>
               </div>
@@ -27,12 +25,42 @@ export default function Home() {
           </div>
           <div className="w-full rounded justify-center items-center">
             <img
-              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674524803/Zhm_Distribuidores/pendones_aiqjpg.jpg"
+              alt="Pendones"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
+                  Pendones
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674524985/Zhm_Distribuidores/carpetas_yhrznm.jpg"
               alt="image"
             />
             <div class="grid justify-items-stretch -mt-8">
               <div class="justify-self-center">
-                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
+                  Carpetas
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-flow-row md:grid-flow-col lg:grid-flow-col gap-4 hover:border-transparent hover:text-teal-500 hover:bg-white mt-8">
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674496475/Zhm_Distribuidores/modelo_tarjeta_iauqsj.jpg"
+              alt="image"
+              className="imagenportafolio"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
                   Tarjetas
                 </button>
               </div>
@@ -40,13 +68,26 @@ export default function Home() {
           </div>
           <div className="w-full rounded justify-center items-center">
             <img
-              src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674524803/Zhm_Distribuidores/pendones_aiqjpg.jpg"
+              alt="Pendones"
+            />
+            <div class="grid justify-items-stretch -mt-8">
+              <div class="justify-self-center">
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
+                  Pendones
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full rounded justify-center items-center">
+            <img
+              src="https://res.cloudinary.com/zhamat-tecnologia/image/upload/v1674524985/Zhm_Distribuidores/carpetas_yhrznm.jpg"
               alt="image"
             />
             <div class="grid justify-items-stretch -mt-8">
               <div class="justify-self-center">
-                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
-                  Tarjetas
+                <button className="w-36 h-10 px-6 m-2 text-lg text-indigo-100 duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 shadow-[0_5px_0_rgb(110,110,114)] hover:shadow-[0_2px_0px_rgb(110,110,114)] ease-out hover:translate-y-1 transition-all ">
+                  Carpetas
                 </button>
               </div>
             </div>
