a8ee32b625aaaac17136e8a352f3a2659b74802a
Willy <wavargas9@misena.edu.co>
2023-01-23 19:00:08 -0500

    portafolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,11 @@ export default function Home() {
         <div className="text-center my-4 text-2xl">Nuestro Portafolio</div>
         <div
           id="portafolio"
-          className="grid grid-flow-row md:grid-flow-col gap-2"
+          className="grid grid-flow-row md:grid-flow-col lg:grid-flow-col gap-4"
         >
-          <div className="flex gap-2">
-            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
-            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
-          </div>
-
-          <div className="flex gap-2">
-            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
-            <div className="grid border border-red-800 rounded-xl tarjeta1  hover:rotate-0" />
-          </div>
+          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
+          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
+          <div className="grid border rounded-xl tarjeta1 rotate-6 hover:rotate-0" />
         </div>
       </Layout>
     </>
