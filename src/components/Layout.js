602f2431f70057d987c291dc19df9f2cebc1af84
Willy <wavargas9@misena.edu.co>
2023-01-27 21:42:53 -0500

    Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -27,16 +27,6 @@ export default function Layout({ title, children }) {
           <div className="flex items-center">
             Carrera 22 No 12B - 32. Bogotá <>Cel.32010000000</>
           </div>
-          <div>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.2244837548825!2d-74.09096450139715!3d4.612284330209046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f996e75c592a7%3A0x95801a2e4ed44a54!2zQ3JhLiAyOCAjMTJCLTQ0LCBMb3MgTcOhcnRpcmVzLCBCb2dvdMOh!5e0!3m2!1ses!2sco!4v1674872870172!5m2!1ses!2sco"
-              width="450"
-              height="200"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            />
-          </div>
         </div>
       </footer>
     </>
